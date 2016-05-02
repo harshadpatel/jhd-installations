3aa096b65e17ec0cfb50c40a642d6c6b8ec8d681
harshadpatel <harshadpatel450@googlemail.com>
2016-05-02 14:36:23 +0100

    knocking off google analytics tag.

--- wp-content/themes/jhd/header.php
+++ wp-content/themes/jhd/header.php
@@ -26,16 +26,6 @@
     <script src="<?php echo get_bloginfo('template_directory'); ?>/lib/dist/js/fancybox/jquery.fancybox.pack.js"></script>
     <script src="<?php echo get_bloginfo('template_directory'); ?>/lib/dist/js/caroufredsel/jquery.carouFredSel-6.2.1-packed.js"></script>
 
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-25576948-1', 'auto');
-        ga('send', 'pageview');
-
-    </script>
 </head>
 <body <?php body_class($post->post_name); ?> id="top">
 
