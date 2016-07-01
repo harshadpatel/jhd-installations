57bdc98227abaf08d7052693b9a732166814375c
harshadpatel <harshadpatel450@googlemail.com>
2016-07-01 02:17:49 +0100

    Completed Case Study section

--- wp-content/themes/jhd/page-case-studies.php
+++ wp-content/themes/jhd/page-case-studies.php
@@ -7,23 +7,12 @@ get_header();
 
 ?>
 
-    <div class="banner">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12">
 
-                    <div class="copy">
-                        <?php the_field('banner_intro'); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <div class="product-items">
         <div class="container">
             <div class="row">
-                <div class="col-xs-12 col-sm-7 center-col-block">
+                <div class="col-xs-12 col-sm-12 center-col-block">
                     <h1 class="subheading"><span><?php the_title(); ?></span></h1>
                     <?php the_field('intro'); ?>
                 </div>
@@ -33,16 +22,15 @@ get_header();
                     <?php if(get_field('casestudy')) : ?>
                         <?php while(has_sub_field('casestudy')) : ?>
 
-                            <div class="col-xs-12 col-md-4 item">
+                            <div class="col-xs-12 col-md-6 item">
                                 <a href="<?php the_sub_field('link');?>">
-                                <h3><?php the_sub_field('type');?></h3>
                                 <div class="copy">
                                     <div class="image">
                                         <img src="<?php the_sub_field('image');?>" class="img-responsive">
                                     </div>
                                     <h2><?php the_sub_field('title');?></h2>
                                     <?php the_sub_field('copy');?>
-                                    <span class="btn btn-cta-light-green"><?php the_sub_field('link_copy');?> &gt;</span>
+                                    <span class="btn-red triangle-orig"><?php the_sub_field('link_copy');?></span>
                                 </div>
                                 </a>
                             </div>
@@ -56,11 +44,6 @@ get_header();
         </div>
     </div>
 
-<?php get_template_part('partials/quotes'); ?>
-
-
-
-<?php get_template_part('partials/contact'); ?>
 
 
 <?php get_footer(); ?>
